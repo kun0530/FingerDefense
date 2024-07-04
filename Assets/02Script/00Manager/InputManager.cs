a0986138790162accf781d4c78fa689cd24e3be8
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-07-04 14:23:48 +0900

    몬스터 드래그 드랍 관련 구현

---
+++ Assets/02Script/00Manager/InputManager.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-1)]
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+    #region Events
+    public event Action<InputAction.CallbackContext> OnClick;
+    public event Action<InputAction.CallbackContext> OnRelease;
+    public event Action<InputAction.CallbackContext> OnDrag;
+    #endregion
+    private Control control;
+    
+    private void Awake()
+    {
+        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        control = new Control();
+    }
+    
+    
+    private void OnEnable()
+    {
+        control.Enable();
+        control.MonsterDrag.Click.performed += context => OnClick?.Invoke(context);
+        control.MonsterDrag.Release.performed += context => OnRelease?.Invoke(context);
+        control.MonsterDrag.Drag.performed += context => OnDrag?.Invoke(context);
+    }
+    
+    private void OnDisable()
+    {
+        control.Disable();
+        control.MonsterDrag.Click.performed -= context => OnClick?.Invoke(context);
+        control.MonsterDrag.Release.performed -= context => OnRelease?.Invoke(context);
+        control.MonsterDrag.Drag.performed -= context => OnDrag?.Invoke(context);
+        
+        
+    }
+
+    
+}
---
+++ Assets/02Script/04System/DragAndDrop.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 offset;
+    private bool isDragging = false;
+    private Transform targetObject;
+    public string draggableTag = "Draggable"; // 드래그 가능한 오브젝트의 태그 설정
+
+    private void OnEnable()
+    {
+        InputManager.Instance.OnClick += OnPointerDown;
+        InputManager.Instance.OnRelease += OnPointerUp;
+        InputManager.Instance.OnDrag += OnPointerDrag;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnClick -= OnPointerDown;
+        InputManager.Instance.OnRelease -= OnPointerUp;
+        InputManager.Instance.OnDrag -= OnPointerDrag;
+    }
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // 태그를 통해 드래그 가능한 오브젝트를 찾아서 드래그 가능 여부를 판단
+    // 인터페이스가 있는지 여부를 가져와야함 
+    private void OnPointerDown(InputAction.CallbackContext context)
+    {
+        Vector2 pointerPosition = GetPointerPosition(context);
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(pointerPosition);
+        Collider2D hitCollider = Physics2D.OverlapPoint(worldPosition);
+
+        if (hitCollider != null && hitCollider.CompareTag(draggableTag))
+        {
+            //if(this==null) return;
+            targetObject = hitCollider.transform;
+            var position = targetObject.position;
+            offset = position - new Vector3(worldPosition.x, worldPosition.y, position.z);
+            isDragging = true;
+        }
+    }
+
+    private void OnPointerUp(InputAction.CallbackContext context)
+    {
+        isDragging = false;
+        targetObject = null;
+    }
+
+    private void OnPointerDrag(InputAction.CallbackContext context)
+    {
+        if (isDragging && targetObject != null)
+        {
+            Vector2 pointerPosition = GetPointerPosition(context);
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(pointerPosition);
+            targetObject.position = new Vector3(worldPosition.x, worldPosition.y, targetObject.position.z) + offset;
+        }
+    }
+
+    private Vector2 GetPointerPosition(InputAction.CallbackContext context)
+    {
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return Touchscreen.current.primaryTouch.position.ReadValue();
+        }
+        else
+        {
+            return Mouse.current.position.ReadValue();
+        }
+    }
+}
