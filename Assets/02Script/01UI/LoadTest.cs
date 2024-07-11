dede0b87681962031ea0866f36b4efcee4e465c5
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-07-11 13:54:09 +0900

    로드 테스트용 업로드

--- Assets/02Script/01UI/LoadTest.cs
+++ Assets/02Script/01UI/LoadTest.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class LoadTest : MonoBehaviour
 {
     public Canvas loadCanvas;
 
-    public 
-    // Start is called before the first frame update
-    void Start()
+
+    private void LoadPrefab()
     {
+        Addressables.InstantiateAsync("Assets/03Prefab/01UI/StageUI.prefab").Completed += handle =>
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                // 프리팹을 로드하고 loadCanvas의 자식으로 설정
+                var instance = handle.Result;
+                instance.transform.SetParent(loadCanvas.transform, false);
+                
+            }
+            else
+            {
+                Debug.LogError("Failed to load prefab.");
+            }
+        };
+        
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        LoadPrefab();
     }
+
 }
