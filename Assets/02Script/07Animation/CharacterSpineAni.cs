f0a7dcc84a8646cf92dc6b5b81543692ff9882a3
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-07-15 10:22:23 +0900

    백업

--- Assets/02Script/07Animation/CharacterSpineAni.cs
+++ Assets/02Script/07Animation/CharacterSpineAni.cs
@@ -4,95 +4,152 @@ using UnityEngine;
 using Spine.Unity;
 using Spine;
 
-
-public enum CharacterAnimState
-{
-    Idle,
-    Walk,
-    Attack,
-    Hit,
-    Die
-}
-
 public class CharacterSpineAni : MonoBehaviour
 {
-    [SerializeField]
-    private SkeletonAnimation[] skeletonAnimation;
+    public SkeletonAnimation[] skeletonAnimation;
     public AnimationReferenceAsset[] characterAnimClip;
+
     private Spine.AnimationState spineAnimationState;
-    
     private CharacterController characterController;
+
+    private CharacterState characterState;
+    private string currentAnimation;
+
+    [Tooltip("케이 캐릭터의 방패에 대한 레이어를 담아두는 변수")]
+    private int originalSortingOrder;
+
+    public enum CharacterState
+    {
+        ATTACK,
+        ATTACK_SHEILD,
+        HIT,
+        IDLE,
+        PASSOUT,
+        RUN
+    }
+
     private void Awake()
     {
-        skeletonAnimation = GetComponentsInChildren<SkeletonAnimation>();
+        if (skeletonAnimation.Length > 2)
+        {
+            originalSortingOrder = skeletonAnimation[2].GetComponent<MeshRenderer>().sortingOrder;
+        }
+        else
+        {
+            Logger.LogWarning("해당 캐릭터는 방패가 없습니다.");
+        }
     }
 
     private void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        spineAnimationState.Event += HandleSpineEvent;
+        characterController = TryGetComponent(out CharacterController controller) ? controller : null;
+        if (skeletonAnimation.Length > 2)
+        {
+            skeletonAnimation[2].AnimationState.Event += HandleSpineEvent;
+        }
+        else
+        {
+            Logger.LogWarning("해당 캐릭터는 방패가 없습니다.");
+        }
     }
+
     private void HandleSpineEvent(TrackEntry trackEntry, Spine.Event e)
     {
-        if(e.Data.Name == "Hit")
+        switch (currentAnimation)
         {
-            ChangeAnimationLayer();
+            case "HIT":
+            case "ATTACK":
+            {
+                if (e.Data.Name == "End")
+                {
+                    if (skeletonAnimation.Length > 2)
+                    {
+                        skeletonAnimation[2].TryGetComponent(out MeshRenderer renderer);
+                        renderer.sortingOrder = originalSortingOrder;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+                break;
         }
     }
-    
-    private void ChangeAnimationLayer()
-    {
-        foreach (var t in skeletonAnimation)
-        {
-            t.AnimationState.SetAnimation(1, characterAnimClip[3], false);
-        }
-    }
+
+
     private void Update()
     {
-        if(characterController)
+        if (!characterController)
+            return;
+
+        if (Input.GetKey(KeyCode.Alpha1))
         {
-            
+            SetAnimation(CharacterState.ATTACK, true, 1f);
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            SetAnimation(CharacterState.ATTACK_SHEILD, true, 1f);
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            SetAnimation(CharacterState.HIT, true, 1f);
+        }
+        else if (Input.GetKey(KeyCode.Alpha4))
+        {
+            SetAnimation(CharacterState.IDLE, true, 1f);
+        }
+        else if (Input.GetKey(KeyCode.Alpha5))
+        {
+            SetAnimation(CharacterState.PASSOUT, true, 1f);
+        }
+        else if (Input.GetKey(KeyCode.Alpha6))
+        {
+            SetAnimation(CharacterState.RUN, true, 1f);
         }
     }
-    
-    public void SetAnimation(CharacterAnimState state)
+
+    public void SetAnimation(CharacterState state, bool loop, float timeScale)
+    {
+        var clip = characterAnimClip[(int)state];
+        if (clip.name.Equals(currentAnimation))
+            return;
+
+        foreach (var skeleton in skeletonAnimation)
+        {
+            skeleton.AnimationState.SetAnimation(0, clip, loop);
+            skeleton.timeScale = timeScale;
+        }
+
+        currentAnimation = clip.name;
+        SetCharacterState(state);
+    }
+
+    public void SetCharacterState(CharacterState state)
     {
         switch (state)
         {
-            case CharacterAnimState.Idle:
-                foreach (var t in skeletonAnimation)
+            case CharacterState.HIT:
+            {
+                if (skeletonAnimation.Length > 2)
                 {
-                    t.AnimationState.SetAnimation(0, characterAnimClip[0], true);
+                    skeletonAnimation[2].TryGetComponent(out MeshRenderer renderer);
+                    renderer.sortingOrder = -1;
                 }
+            }
                 break;
-            case CharacterAnimState.Walk:
-                foreach (var t in skeletonAnimation)
+            case CharacterState.ATTACK:
+            {
+                if (skeletonAnimation.Length > 2)
                 {
-                    t.AnimationState.SetAnimation(0, characterAnimClip[1], true);
+                    skeletonAnimation[2].TryGetComponent(out MeshRenderer renderer);
+                    renderer.sortingOrder = 0;
                 }
-                break;
-            case CharacterAnimState.Attack:
-                foreach (var t in skeletonAnimation)
-                {
-                    t.AnimationState.SetAnimation(0, characterAnimClip[2], true);
-                }
-                break;
-            case CharacterAnimState.Hit:
-                foreach (var t in skeletonAnimation)
-                {
-                    t.AnimationState.SetAnimation(0, characterAnimClip[3], true);
-                }
-                break;
-            case CharacterAnimState.Die:
-                foreach (var t in skeletonAnimation)
-                {
-                    t.AnimationState.SetAnimation(0, characterAnimClip[4], true);
-                }
+
+            }
                 break;
         }
     }
-    
-    
-    
-    
 }
+    
+
