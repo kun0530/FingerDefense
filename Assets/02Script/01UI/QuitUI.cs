3ba1c9b336d9db19a4d4fb951672b5e4f7f9e4b9
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-07-24 14:53:26 +0900

    [정상화] 종료 패널 UI 상태에서 취소 버튼을 눌러도 게임 진행이 안되던 문제 수정

--- Assets/02Script/01UI/QuitUI.cs
+++ Assets/02Script/01UI/QuitUI.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class QuitUI : MonoBehaviour
 {
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+    
     public void OnClickQuit()
     {
         Application.Quit();
