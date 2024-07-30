302d0fa92a6281ff9436cd413695b858374f64fd
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-07-30 21:19:51 +0900

    닉네임 관련 추가 제한 로직 추가 & 텍스트 관련 애니메이션 추가

--- Assets/02Script/01UI/00Main/NickSettingUI.cs
+++ Assets/02Script/01UI/00Main/NickSettingUI.cs
@@ -1,7 +1,8 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Text.RegularExpressions;
+using DG.Tweening;
 
 public class NickSettingUI : MonoBehaviour
 {
@@ -15,6 +16,11 @@ public class NickSettingUI : MonoBehaviour
     public bool isComplete = false;
     public MainUI mainUI;
     private GameManager gameManager;
+    private readonly Regex koreanFullSyllablesRegex = new("^[가-힣a-zA-Z0-9]*$");
+    private string userId;
+
+    public TextMeshProUGUI noticeText;
+    private Vector3 noticeTextInitialPosition;
     
     public void Start()
     {
@@ -22,11 +28,27 @@ public class NickSettingUI : MonoBehaviour
         
         confirmButton.onClick.AddListener(OnClickConfirm);
         cancelButton.onClick.AddListener(OnClickCancel);
-        confirmNickButton.onClick.AddListener(OnClickConfirmNick); 
+        confirmNickButton.onClick.AddListener(OnClickConfirmNick);
+        
+        noticeTextInitialPosition = noticeText.transform.localPosition;
     }
     
     private void OnClickConfirm()
     {
+        userId = inputField.text;
+        if(userId.Length is < 4 or > 10)
+        {
+            ShowNotice("닉네임은 4자 이상 10자 이하로 입력해주세요.");
+            Logger.Log("닉네임은 4자 이상 10자 이하로 입력해주세요.");
+            return;
+        }
+        if(!koreanFullSyllablesRegex.IsMatch(userId))
+        {
+            ShowNotice("닉네임은 한글 음절, 영어, 숫자만 입력 가능합니다.");
+            Logger.Log("닉네임은 한글 음절, 영어, 숫자만 입력 가능합니다.");
+            return;
+        }
+        
         gameManager.PlayerName = inputField.text;
         nickCheckUI.SetActive(true);
         nickNameText.text = gameManager.PlayerName;
@@ -36,6 +58,7 @@ public class NickSettingUI : MonoBehaviour
     private void OnClickCancel()
     {
         nickCheckUI.SetActive(false);
+        gameManager.PlayerName = "";
         Logger.Log($"{gameManager.PlayerName}으로 설정되었습니다.");
     }
 
@@ -47,5 +70,16 @@ public class NickSettingUI : MonoBehaviour
         Logger.Log("닉네임 설정이 완료되었습니다.");
     }
     
-    
+    private void ShowNotice(string message)
+    {
+        noticeText.text = message;
+        noticeText.color = new Color(noticeText.color.r, noticeText.color.g, noticeText.color.b, 1);
+        noticeText.transform.localPosition = noticeTextInitialPosition;
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.AppendInterval(1.5f);
+        sequence.Append(noticeText.DOFade(0, 1f));
+        sequence.Join(noticeText.transform.DOLocalMoveY(noticeTextInitialPosition.y + 50, 1f).SetEase(Ease.InQuad));
+        sequence.Play();
+    }
 }
