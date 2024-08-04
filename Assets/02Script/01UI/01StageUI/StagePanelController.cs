372c8c6995bf37e51633d638ae495e8f8d81b617
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-08-04 12:20:18 +0900

    메인 씬 스테이지 패널 관련 추가 수정

--- Assets/02Script/01UI/01StageUI/StagePanelController.cs
+++ Assets/02Script/01UI/01StageUI/StagePanelController.cs
@@ -8,13 +8,13 @@ public class StagePanelController : MonoBehaviour, IBeginDragHandler, IDragHandl
     public RectTransform stagePanel;
     public float scaleFactor = 0.7f;
     public float animationDuration = 0.5f;
-    private readonly int[] leftPaddings = { 200, 10, -180, -380, -730 };
+    private readonly int[] leftPaddings = { 200,-100,-375,-650,-1100 };
 
     private int currentIndex = 0;
     private Vector2 dragStartPosition;
     private HorizontalLayoutGroup layoutGroup;
-    
-    void Start()
+
+    private void Start()
     {
         layoutGroup = stagePanel.GetComponent<HorizontalLayoutGroup>();
         
@@ -35,13 +35,14 @@ public class StagePanelController : MonoBehaviour, IBeginDragHandler, IDragHandl
     {
         Vector2 dragDelta = eventData.position - dragStartPosition;
 
-        if (dragDelta.x > 0 && currentIndex > 0)
+        switch (dragDelta.x)
         {
-            currentIndex--;
-        }
-        else if (dragDelta.x < 0 && currentIndex < leftPaddings.Length - 1)
-        {
-            currentIndex++;
+            case > 0 when currentIndex > 0:
+                currentIndex--;
+                break;
+            case < 0 when currentIndex < leftPaddings.Length - 1:
+                currentIndex++;
+                break;
         }
 
         UpdatePadding();
@@ -93,6 +94,8 @@ public class StagePanelController : MonoBehaviour, IBeginDragHandler, IDragHandl
             rect.pivot = new Vector2(0.5f, 0.5f);
             rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, 0); 
         }
+
+        return;
 
         void SetButtonsInteractable(Transform slot, bool isInteractable)
         {
