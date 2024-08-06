5089d34677e2da74b9cd201e43a3b24ee0f5c6eb
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-08-06 18:06:12 +0900

    ShopTable 수정

--- Assets/02Script/01UI/00Main/ShopSettingUI.cs
+++ Assets/02Script/01UI/00Main/ShopSettingUI.cs
@@ -20,12 +20,46 @@ public class ShopSettingUI : MonoBehaviour
     public TextMeshProUGUI[] diamondBonusTexts;
     public TextMeshProUGUI[] goldBonusTexts;
     
-    private void Start()
+    private ShopTable shopTable;
+    
+    private bool dataLoaded = false;
+    private void OnEnable()
     {
-        diamondPayTexts[0].text=DataTableManager.Get<StringTable>(DataTableIds.String).Get(90051);
+        if (!dataLoaded)
+        {
+            LoadData();
+            dataLoaded = true;
+        }
+    }
+    private void LoadData()
+    {
+        shopTable = DataTableManager.Get<ShopTable>(DataTableIds.Shop);
+        if (shopTable == null)
+        {
+            Logger.LogError("ShopTable is null");
+            return;
+        }
+        SetTextElements();
+    }
+    private void SetTextElements()
+    {
+        diamondPayTexts[0].text=shopTable.Get(90051);
+        diamondPayTexts[1].text=shopTable.Get(90061);
+        diamondPayTexts[2].text=shopTable.Get(90081);
+        diamondPayTexts[3].text=shopTable.Get(90101);
+        diamondPayTexts[4].text=shopTable.Get(90121);
+        diamondPayTexts[5].text=shopTable.Get(90141);
+        goldPayTexts[0].text=shopTable.Get(90161);
+        goldPayTexts[1].text=shopTable.Get(90171);
+        goldPayTexts[2].text=shopTable.Get(90181);
+        goldPayTexts[3].text=shopTable.Get(90191);
+        goldPayTexts[4].text=shopTable.Get(90201);
+        goldPayTexts[5].text=shopTable.Get(90211);
         
     }
 
+
+    
     private void Update()
     {
         UpdatePlayerInfo();
--- Assets/02Script/03DataTable/ShopTable.cs
+++ Assets/02Script/03DataTable/ShopTable.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 using CsvHelper;
 using System.IO;
 using System.Globalization;
+using System.Text;
 using UnityEngine.AddressableAssets;
 
 public class ShopData
 {
     public int Id { get; set; }
-    public int NameId { get; set; }
-    public int DescId { get; set; }
-    public int PayType { get; set; }
-    public int Price { get; set; }
-    public int ItemId { get; set; }
-    public int Amount { get; set; }
-    public int AssetNo { get; set; }
+    public string Text { get; set; }
 }
 
 public class ShopTable : DataTable
 {
-    public Dictionary<int,ShopData>table=new Dictionary<int, ShopData>();
+    public Dictionary<int,string> table = new Dictionary<int, string>();
     
     public bool IsExist(int id)
     {
         return table.ContainsKey(id);
     }
     
-    public ShopData Get(int id)
+    public string Get(int id)
     {
         return table.GetValueOrDefault(id);
     }
@@ -35,13 +30,17 @@ public class ShopTable : DataTable
     public override void Load(string path)
     {
         path = string.Format(FormatPath, path);
+
         var textAsset = Addressables.LoadAssetAsync<TextAsset>(path).WaitForCompletion();
-        using var reader = new StringReader(textAsset.text);
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        var records = csv.GetRecords<ShopData>();
+        //var textAsset = Resources.Load<TextAsset>(path);
+
+        using var reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(textAsset.text)), Encoding.UTF8);
+        using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csvReader.GetRecords<ShopData>();
         foreach (var record in records)
         {
-            table.Add(record.Id, record);
+            string processedText = record.Text.Replace("\\n", "\n");
+            table.TryAdd(record.Id, processedText);
         }
     }
 }
