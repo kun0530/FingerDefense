81caedfe1105a4226eb089a020d24f0697f0a8d7
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-08-13 17:34:57 +0900

    스트링 테이블도 받아올수 있도록 추가 예정

--- Assets/02Script/01UI/00Main/01UpgradePanel/DragInfoSlot.cs
+++ Assets/02Script/01UI/00Main/01UpgradePanel/DragInfoSlot.cs
@@ -7,13 +7,20 @@ public class DragInfoSlot : MonoBehaviour
     public TextMeshProUGUI monsterCost;
     public Button buyButton;
     
+    private StringTable stringTable;
+    
+    private void Awake()
+    {
+        stringTable ??= DataTableManager.Get<StringTable>(DataTableIds.String);
+    }
+    
     private void Start()
     {
         UpdateCostColor();
-        buyButton.onClick.AddListener(CheakBuy);
+        buyButton.onClick.AddListener(CheckBuy);
     }
 
-    private void CheakBuy()
+    private void CheckBuy()
     {
         var cost = int.Parse(monsterCost.text);
         var playerGold = GameManager.instance.ResourceManager.Gold;
@@ -22,14 +29,17 @@ public class DragInfoSlot : MonoBehaviour
         {
             ModalWindow.Create()
                 .SetHeader("구매 확인")
-                .SetBody("해당 몬스터를 구매하시겠습니까?")
+                .SetBody($"{cost}골드를 사용해서 해당 몬스터의 드래그기능을 구매하시겠습니까?")
                 .AddButton("확인", () =>
                 {
                     GameManager.instance.ResourceManager.Gold -= cost;
                     UpdateInteractive();
                     this.gameObject.SetActive(false);
                 })
-                .AddButton("취소", () => {this.gameObject.SetActive(false); })
+                .AddButton("취소", () =>
+                {
+                    this.gameObject.SetActive(false);
+                })
                 .Show();
         }
         else
@@ -37,7 +47,10 @@ public class DragInfoSlot : MonoBehaviour
             ModalWindow.Create()
                 .SetHeader("구매 실패")
                 .SetBody("골드가 부족합니다.")
-                .AddButton("확인", () => {this.gameObject.SetActive(false); })
+                .AddButton("확인", () =>
+                {
+                    this.gameObject.SetActive(false);
+                })
                 .Show();
         }
     }
