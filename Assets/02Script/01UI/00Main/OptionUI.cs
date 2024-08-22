920d1fc7aa65ec9a7a8d01c72fd4c77c3afbf500
Park Hyeong-Gon <h.gon0530@gmail.com>
2024-08-22 11:30:59 +0900

    - 사운드 매니저 추가

--- Assets/02Script/01UI/00Main/OptionUI.cs
+++ Assets/02Script/01UI/00Main/OptionUI.cs
@@ -21,77 +21,77 @@ public class OptionUI : MonoBehaviour
     public AudioMixer AudioMixer;
     
     
-    public void Start()
-    {
-        bgmSlider.value = PlayerPrefs.GetFloat("BgmVolume", 0.5f);
-        sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
-        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
-        bgmToggle.isOn = PlayerPrefs.GetInt("BgmMute", 0) == 1;
-        sfxToggle.isOn = PlayerPrefs.GetInt("SfxMute", 0) == 1;
-        MasterToggle.isOn = PlayerPrefs.GetInt("MasterMute", 0) == 1;
-        vibrationToggle.isOn = PlayerPrefs.GetInt("Vibration", 1) == 1;
+    // public void Start()
+    // {
+    //     bgmSlider.value = PlayerPrefs.GetFloat("BgmVolume", 0.5f);
+    //     sfxSlider.value = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
+    //     masterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0.5f);
+    //     bgmToggle.isOn = PlayerPrefs.GetInt("BgmMute", 0) == 1;
+    //     sfxToggle.isOn = PlayerPrefs.GetInt("SfxMute", 0) == 1;
+    //     MasterToggle.isOn = PlayerPrefs.GetInt("MasterMute", 0) == 1;
+    //     vibrationToggle.isOn = PlayerPrefs.GetInt("Vibration", 1) == 1;
         
-        bgmSlider.onValueChanged.AddListener(delegate { SetBgmVolume(); });
-        sfxSlider.onValueChanged.AddListener(delegate { SetSfxVolume(); });
-        masterSlider.onValueChanged.AddListener(delegate { SetMasterVolume(); });
-        bgmToggle.onValueChanged.AddListener(delegate { SetBgmMute(); });
-        sfxToggle.onValueChanged.AddListener(delegate { SetSfxMute(); });
-        MasterToggle.onValueChanged.AddListener(delegate { SetMasterMute(); });
-        vibrationToggle.onValueChanged.AddListener(delegate { SetVibration(); });
+    //     bgmSlider.onValueChanged.AddListener(delegate { SetBgmVolume(); });
+    //     sfxSlider.onValueChanged.AddListener(delegate { SetSfxVolume(); });
+    //     masterSlider.onValueChanged.AddListener(delegate { SetMasterVolume(); });
+    //     bgmToggle.onValueChanged.AddListener(delegate { SetBgmMute(); });
+    //     sfxToggle.onValueChanged.AddListener(delegate { SetSfxMute(); });
+    //     MasterToggle.onValueChanged.AddListener(delegate { SetMasterMute(); });
+    //     vibrationToggle.onValueChanged.AddListener(delegate { SetVibration(); });
         
-    }
+    // }
 
-    public void Update()
-    {
-    }
+    // public void Update()
+    // {
+    // }
 
-    private void SetBgmVolume()
-    {
-        float volume = bgmSlider.value;
-        AudioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("BGM", volume);
-    }
+    // private void SetBgmVolume()
+    // {
+    //     float volume = bgmSlider.value;
+    //     AudioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    //     PlayerPrefs.SetFloat("BGM", volume);
+    // }
 
-    private void SetSfxVolume()
-    {
-        float volume = sfxSlider.value;
-        AudioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("SFX", volume);
-    }
+    // private void SetSfxVolume()
+    // {
+    //     float volume = sfxSlider.value;
+    //     AudioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    //     PlayerPrefs.SetFloat("SFX", volume);
+    // }
 
-    private void SetMasterVolume()
-    {
-        float volume = masterSlider.value;
-        AudioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("Master", volume);
-    }
+    // private void SetMasterVolume()
+    // {
+    //     float volume = masterSlider.value;
+    //     AudioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    //     PlayerPrefs.SetFloat("Master", volume);
+    // }
 
-    private void SetBgmMute()
-    {
-        int mute = bgmToggle.isOn ? 1 : 0;
-        AudioMixer.SetFloat("BGM", mute == 1 ? -80 : Mathf.Log10(bgmSlider.value) * 20);
-        PlayerPrefs.SetInt("BGM", mute);
-    }
+    // private void SetBgmMute()
+    // {
+    //     int mute = bgmToggle.isOn ? 1 : 0;
+    //     AudioMixer.SetFloat("BGM", mute == 1 ? -80 : Mathf.Log10(bgmSlider.value) * 20);
+    //     PlayerPrefs.SetInt("BGM", mute);
+    // }
 
-    private void SetSfxMute()
-    {
-        int mute = sfxToggle.isOn ? 1 : 0;
-        AudioMixer.SetFloat("SFX", mute == 1 ? -80 : Mathf.Log10(sfxSlider.value) * 20);
-        PlayerPrefs.SetInt("SFX", mute);
-    }
+    // private void SetSfxMute()
+    // {
+    //     int mute = sfxToggle.isOn ? 1 : 0;
+    //     AudioMixer.SetFloat("SFX", mute == 1 ? -80 : Mathf.Log10(sfxSlider.value) * 20);
+    //     PlayerPrefs.SetInt("SFX", mute);
+    // }
 
-    private void SetMasterMute()
-    {
-        int mute = MasterToggle.isOn ? 1 : 0;
-        AudioMixer.SetFloat("Master", mute == 1 ? -80 : Mathf.Log10(masterSlider.value) * 20);
-        PlayerPrefs.SetInt("Master", mute);
-    }
+    // private void SetMasterMute()
+    // {
+    //     int mute = MasterToggle.isOn ? 1 : 0;
+    //     AudioMixer.SetFloat("Master", mute == 1 ? -80 : Mathf.Log10(masterSlider.value) * 20);
+    //     PlayerPrefs.SetInt("Master", mute);
+    // }
 
-    private void SetVibration()
-    {
-        int vibration = vibrationToggle.isOn ? 1 : 0;
-        PlayerPrefs.SetInt("Vibration", vibration);
-    }
+    // private void SetVibration()
+    // {
+    //     int vibration = vibrationToggle.isOn ? 1 : 0;
+    //     PlayerPrefs.SetInt("Vibration", vibration);
+    // }
     
     public void OnClickBack()
     {
